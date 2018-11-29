c0aedef0d511ce8e87e4898dc8f30fc75dde94c0
zyx_test <zhaotester@163.com>
2018-11-29 16:00:25 +0800

    添加公共页面操作的方法

---
+++ PageObject/common_page.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/10 14:26
+# @Author  :  'zyx'
+# @Email   : 458757014@qq.com
+# @File    : MyLogger.py
+# @Software: PyCharm Community Edition
+
+from Common.BasePage import BasePage
+from PageLocator.common_locator import CommonLocator
+
+class CommonPage(CommonLocator,BasePage):
+    pass
